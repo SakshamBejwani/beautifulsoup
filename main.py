85a6bb4a30923511d46a91fcdff8c0621a4c0bc1
Saksham Bejwani <sakshambejwani@gmail.com>
2021-04-30 17:37:21 +0530

    filtering skills

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 
+print("Enter some skills you don't want")
+skill_select = input('>')
+
 job_names = soup.find_all('li', class_ ='clearfix job-bx wht-shd-bx')
 for job in job_names:
     time_posted=job.find('span',class_ ='sim-posted').text.strip()
     if'few' in time_posted:
         company_name = job.find('h3', class_ ='joblist-comp-name').text.strip()
         skills= job.find('span', class_ = 'srp-skills').text.strip()
+        more_info = job.header.h2.a['href']
         print(f'''
         Company Name: {company_name}
         Required Skills: {skills}
-        Age: {time_posted}
+        More Info: {more_info}
         ''')
         print('')
